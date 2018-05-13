60ea93779c8556e175371ec66717febccd64d6c4
isaacgr <isaacgrowell@gmail.com>
2018-05-12 22:57:55 -0400

    Delte calculator app

--- react-calculator-app/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CalculatorApp from './components/CalculatorApp';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
-ReactDOM.render(<CalculatorApp />, document.getElementById('app'));
